7d3a8f105d4f62c8d2aae50b42d201ca32e3a5fd
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2022-08-10 07:10:37 +0100

    test

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
-import { createStore, createLogger } from "vuex";
-import SecureLS from "secure-ls";
-import createPersistedState from "vuex-persistedstate";
+import { createStore } from "vuex";
+// import SecureLS from "secure-ls";
+// import createPersistedState from "vuex-persistedstate";
 import userStore from "./modules/userStore";
 import documentStore from "./modules/documentStore";
 import notarysessionStore from "./modules/notarysessionStore";
 
-const ls = new SecureLS({ isCompression: true });
-const debug = process.env.NODE_ENV !== "production";
+// const ls = new SecureLS({ isCompression: true });
+// const debug = process.env.NODE_ENV !== "production";
 
 export default createStore({
   modules: {
@@ -17,13 +17,13 @@ export default createStore({
 
   // strict: debug,
   plugins: [
-    debug ? createLogger() : [],
-    createPersistedState({
-      storage: {
-        getItem: (key) => ls.get(key),
-        setItem: (key, value) => ls.set(key, value),
-        removeItem: (key) => ls.remove(key),
-      },
-    }),
+    // debug ? createLogger() : [],
+    // createPersistedState({
+    //   storage: {
+    //     getItem: (key) => ls.get(key),
+    //     setItem: (key, value) => ls.set(key, value),
+    //     removeItem: (key) => ls.remove(key),
+    //   },
+    // }),
   ],
 });
